f80ece2c9c64b670d4f59213518b6e77c400fdc9
heerasheemar <66547950+heerasheemar@users.noreply.github.com>
2023-04-27 22:23:52 +0530

    Update SecretClientConfiguration.java

--- src/main/java/com/sapient/keyvaultvaluereader/KeyVaultValueReader/configs/SecretClientConfiguration.java
+++ src/main/java/com/sapient/keyvaultvaluereader/KeyVaultValueReader/configs/SecretClientConfiguration.java
@@ -1,25 +1,25 @@
-package com.sapient.keyvaultvaluereader.KeyVaultValueReader.configs;
+// package com.sapient.keyvaultvaluereader.KeyVaultValueReader.configs;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import com.azure.identity.DefaultAzureCredentialBuilder;
-import com.azure.security.keyvault.secrets.SecretClient;
-import com.azure.security.keyvault.secrets.SecretClientBuilder;
+// import com.azure.identity.DefaultAzureCredentialBuilder;
+// import com.azure.security.keyvault.secrets.SecretClient;
+// import com.azure.security.keyvault.secrets.SecretClientBuilder;
 
-@Configuration
-public class SecretClientConfiguration {
+// @Configuration
+// public class SecretClientConfiguration {
 	
-	@Value("${spring.cloud.azure.keyvault.secret.endpoint}")
-	String endpoint;
+// 	@Value("${spring.cloud.azure.keyvault.secret.endpoint}")
+// 	String endpoint;
 
-    @Bean
-    public SecretClient createSecretClient() {
-        return new SecretClientBuilder()
-            .vaultUrl(endpoint)
-            .credential(new DefaultAzureCredentialBuilder().build())
-            .buildClient();
-    }
+//     @Bean
+//     public SecretClient createSecretClient() {
+//         return new SecretClientBuilder()
+//             .vaultUrl(endpoint)
+//             .credential(new DefaultAzureCredentialBuilder().build())
+//             .buildClient();
+//     }
 
-}
+// }
