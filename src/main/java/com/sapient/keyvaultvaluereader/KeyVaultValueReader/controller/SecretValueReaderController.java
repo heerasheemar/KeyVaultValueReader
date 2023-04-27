54a316c1e78996d462b42e0d2baeb5c4c3a95e71
heerasheemar <66547950+heerasheemar@users.noreply.github.com>
2023-04-27 22:24:32 +0530

    Update SecretValueReaderController.java

--- src/main/java/com/sapient/keyvaultvaluereader/KeyVaultValueReader/controller/SecretValueReaderController.java
+++ src/main/java/com/sapient/keyvaultvaluereader/KeyVaultValueReader/controller/SecretValueReaderController.java
@@ -10,16 +10,16 @@ import com.azure.security.keyvault.secrets.SecretClient;
 public class SecretValueReaderController {
 	
 	// Spring Cloud Azure will automatically inject SecretClient in your ApplicationContext.
-	@Autowired
-    private SecretClient secretClient;
+// 	@Autowired
+//     private SecretClient secretClient;
 	
 	@GetMapping("/")
 	public String welcome() {
 		return "Secret Value Reader App to read from Azure Key Vault";
 	}
 	
-	@GetMapping("/secret")
-	public String getSecretValue() {
-		return "Secret Value from Azure Key Vault is: "+secretClient.getSecret("servicesecret").getValue();
-	}
+// 	@GetMapping("/secret")
+// 	public String getSecretValue() {
+// 		return "Secret Value from Azure Key Vault is: "+secretClient.getSecret("servicesecret").getValue();
+// 	}
 }
